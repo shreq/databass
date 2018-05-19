32cd8a28c48e28b89edb8bf25783efa58e1e1a60
shreq <37081480+shreq@users.noreply.github.com>
2018-05-19 17:10:12 +0200

    dodano rzeczy

--- restauracje_kwerendy.sql
+++ restauracje_kwerendy.sql
@@ -98,9 +98,17 @@ where p.pensja > (select MIN(xp.pensja) from restauracja..personel as xp, restau
 	  s.nazwa != 'kierownik' AND s.stanowiskoID = p.stanowiskoID
 go
 
+-- 12 ++
+-- wyswietlenie ile zarobil dany pracownik od momentu zatrudnienia (zakladamy ze pensja sie nie zmieniala)
+select r.nazwa, p.imie+' '+p.nazwisko as [imie nazwisko], DATEDIFF(year, p.dataZatr, GETDATE()) as [lat pracy], DATEDIFF(month, p.dataZatr, GETDATE())*p.pensja as [zarobil od zatrudnienia]
+from restauracja..personel as p, restauracja..restauracje as r
+where p.restauracjaID = r.restauracjaID
+order by [zarobil od zatrudnienia] desc
 
-
-
+-- 13 ++
+-- wyswietlenie w jakim wieku zatrudnil sie dany pracownik
+select p.imie, p.nazwisko, FLOOR(DATEDIFF(day, p.dataUr, p.dataZatr)/365.242199) as [wiek przy zatrudnieniu]
+from restauracja..personel as p
 
 
 

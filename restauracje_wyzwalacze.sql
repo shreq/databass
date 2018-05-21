945022ce5350b62f7d97cc9c131b60efa7faf011
shreq <shrek.kresh@o2.pl>
2018-05-21 22:36:20 +0200

    koniec

--- restauracje_wyzwalacze.sql
+++ restauracje_wyzwalacze.sql
@@ -1,10 +1,11 @@
+-- use master
 use restauracja
 go
 
 -- wyzwalacz usuwajacy pracownikow
 -- instead of delete
-drop trigger restauracja_usunPracownika
-go
+
+--drop trigger restauracja_usunPracownika
 create trigger restauracja_usunPracownika
 on restauracja..personel
 instead of delete
@@ -19,15 +20,15 @@ go
 
 delete from restauracja..personel
 where personelID = 1 or personelID = 2
-select * from restauracja..personel
-select * from restauracja..personel_archiw
+--select * from restauracja..personel
+select personelID, imie, nazwisko, dataZwolnienia from restauracja..personel_archiw
 go
 
 -- wyzwalacz sprawdzajacy cene dania przy jego dodawaniu do menu
 -- jezeli cena dania przekracza maksymalna cene okreslona dla restauracji, to nie zostanie dodane do menu
 -- instead of insert
-drop trigger sprawdzCene
-go
+
+--drop trigger sprawdzCene
 create trigger sprawdzCene
 on restauracja..menudania
 instead of insert
@@ -60,6 +61,7 @@ insert into restauracja..dania values (101, 'danie testowe 2', 15);
 insert into restauracja..menudania values (100, 1, 100);
 insert into restauracja..menudania values (101, 1, 101);
 go
+
 select d.danieID, d.nazwa, d.cena
 from restauracja..menudania as md, restauracja..dania as d, restauracja..menu as m
 where md.danieID = d.danieID and md.menuID = m.menuID and m.menuID = 1
@@ -69,8 +71,8 @@ go
 -- sprawdza, czy pensje pracownikow mieszcza sie w nowym przedziale
 -- jezeli nie, to zmienia wartosc pensji
 -- after update
-drop trigger aktualizujPensje
-go
+
+--drop trigger aktualizujPensje
 create trigger aktualizujPensje
 on restauracja..stanowiska
 after update
@@ -112,13 +114,16 @@ update restauracja..stanowiska
 set placa_max = 1100.00
 where nazwa = 'kelner'
 go
+
 update restauracja..stanowiska
 set placa_min = 2500.00
 where nazwa = 'szef kuchni'
 go
+
 select p.personelID, p.imie, p.nazwisko, p.pensja, s.nazwa
 from restauracja..personel as p, restauracja..stanowiska as s
 where p.stanowiskoID = s.stanowiskoID and (s.nazwa = 'kelner' or s.nazwa = 'szef kuchni')
+go
 
 use master
 go
